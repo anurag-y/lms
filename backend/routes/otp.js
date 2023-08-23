5e5c96bdd22bc18f7acad9558fd5cbfbd8ec1661
Anurag <anurag.yadav@iitg.ac.in>
2023-08-23 20:07:30 +0530

    OTP verifcation implemented

--- backend/routes/otp.js
+++ backend/routes/otp.js
@@ -3,12 +3,14 @@
 const express = require('express');
 const router = express.Router();
 const nodemailer = require('nodemailer');
+var lastOTPtime = 0;
+var lastOTP=11111;
 require('dotenv').config();
 const gmailpassword = process.env.gmail_password;
 router.post('/send-otp/', (req, res) => {
     const randomOTP = Math.floor(10000 + Math.random() * 90000); // Generates OTP between 10000 and 99999
+    lastOTP=randomOTP;
     const userEmailAddress = req.body.userEmail;
-    console.log('email', userEmailAddress);
 
     var transporter = nodemailer.createTransport({
         service: 'gmail',
@@ -24,18 +26,36 @@ router.post('/send-otp/', (req, res) => {
         subject: 'OTP Verification',
         text: `Your OTP is: ${randomOTP}`
     };
+    if(Date.now() - lastOTPtime < 180000) {
+        res.json({ success: false, message: 'Please wait for 3 minutes before sending another OTP.' });
+        return;
+    }
 
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
             console.log('Error sending email:', error);
-            res.json({ success: false });
+            res.json({ success: false, message: 'Some error occurred while sending OTP. Please try again.'});
         } else {
+            lastOTPtime = Date.now();
             console.log('Email sent:', info.response);
-            // 
-            res.json({ success: true });
+            res.json({ success: true, message: 'OTP sent successfully. Valid for 5 minutes.' });
         }
     });
 });
+
+router.post('/verify-otp/', (req, res) => {
+    const otp = req.body.enteredOTP;
+    if(Date.now() - lastOTPtime > 300000) {
+        res.json({ success: false, message: 'OTP expired. Please try again.' });
+        return;
+    }
+    if(otp == lastOTP) {
+        res.json({ success: true, message: 'OTP verified successfully.' });
+    } else {
+        res.json({ success: false, message: 'Incorrect OTP. Please try again.' });
+    }
+});
+
 
 
 
--- frontend/public/borrow.js
+++ frontend/public/borrow.js
@@ -1,5 +1,7 @@
 var userDataElement = document.getElementById('user-data');
 var userEmail = userDataElement.getAttribute('data-user-email');
+var enteredOTP;
+
 document.getElementById('otp-button').addEventListener('click', async () => {
     
     console.log('email', userEmail);
@@ -16,6 +18,40 @@ document.getElementById('otp-button').addEventListener('click', async () => {
     if (result.success) {
         alert('OTP has been sent to your email.');
     } else {
-        alert('Error sending OTP. Please try again later.');
+        alert(result.message);
     }
+});
+
+function focusNextInput(currentInput) {
+    const maxLength = parseInt(currentInput.getAttribute('maxlength'));
+    const currentLength = currentInput.value.length;
+    
+    if (currentLength === maxLength) {
+      const nextInput = currentInput.nextElementSibling;
+      if (nextInput) {
+        nextInput.focus();
+      }
+    }
+    
+    const otpInputs = document.querySelectorAll('.otp-input');
+    let otpValue = '';
+    
+    otpInputs.forEach(input => {
+      otpValue += input.value;
+    });
+    enteredOTP = otpValue;    
+    
+  }
+document.getElementById('verify-otp-button').addEventListener('click', async () => {
+    console.log('enteredOTP', enteredOTP);
+    const response = await fetch('/otp/verify-otp', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ enteredOTP })
+    });
+
+    const result = await response.json();
+    alert(result.message);
 });
